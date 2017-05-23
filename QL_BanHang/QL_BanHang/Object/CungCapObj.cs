7346638d64832287b61f53320f99a323b7f01a87
VuThuy22 <vthuy2222@gmail.com>
2017-05-23 16:18:08 +0700

    Nhà cung cấp

---
+++ QL_BanHang/QL_BanHang/Object/CungCapObj.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_BanHang.Object
+{
+    class CungCapObj
+    {
+        string MaNCC, MaMH, SoLuong;
+
+        public string MaMH1
+        {
+            get
+            {
+                return MaMH;
+            }
+
+            set
+            {
+                MaMH = value;
+            }
+        }
+
+        public string MaNCC1
+        {
+            get
+            {
+                return MaNCC;
+            }
+
+            set
+            {
+                MaNCC = value;
+            }
+        }
+
+        public string SoLuong1
+        {
+            get
+            {
+                return SoLuong;
+            }
+
+            set
+            {
+                SoLuong = value;
+            }
+        }
+        public CungCapObj() { }
+        public CungCapObj(string MaNCC, string MaMH, string SoLuong)
+        {
+            this.MaNCC = MaNCC;
+            this.MaMH = MaMH;
+            this.SoLuong = SoLuong;
+        }
+    }
+}
