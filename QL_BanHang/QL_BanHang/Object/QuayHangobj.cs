9dabc3cfe3cb2dd6431548c0b3fe54dbebaadbc8
manhhiep1996 <manhhiep1996@gmail.com>
2017-05-23 14:12:53 -0700

    [Hiep] form quầy hàng

---
+++ QL_BanHang/QL_BanHang/Object/QuayHangobj.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_BanHang.Object
+{
+    class QuayHangObj
+    {
+        string MaQH, TenQuayHang, DiaChi, MaNQL;
+
+        public string DiaChi1
+        {
+            get
+            {
+                return DiaChi;
+            }
+
+            set
+            {
+                DiaChi = value;
+            }
+        }
+
+        public string MaNQL1
+        {
+            get
+            {
+                return MaNQL;
+            }
+
+            set
+            {
+                MaNQL = value;
+            }
+        }
+
+        public string MaQH1
+        {
+            get
+            {
+                return MaQH;
+            }
+
+            set
+            {
+                MaQH = value;
+            }
+        }
+
+        public string TenQuayHang1
+        {
+            get
+            {
+                return TenQuayHang;
+            }
+
+            set
+            {
+                TenQuayHang = value;
+            }
+        }
+
+        public QuayHangObj() { }
+        public QuayHangObj(string MaQH, string TenQuayHang, string DiaChi, string MaNQL)
+        {
+            this.MaQH = MaQH;
+            this.TenQuayHang = TenQuayHang;
+            this.DiaChi = DiaChi;
+            this.MaNQL = MaNQL;
+        }
+    }
+}
---
+++ QL_BanHang/QL_BanHang/View/frmQuayHang.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace QL_BanHang.View
+{
+    partial class frmQuayHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTroLai = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvQuayHang = new System.Windows.Forms.DataGridView();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.txtTenQH = new System.Windows.Forms.TextBox();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaNQL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMaQH = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQuayHang)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTroLai
+            // 
+            this.btnTroLai.Location = new System.Drawing.Point(867, 492);
+            this.btnTroLai.Name = "btnTroLai";
+            this.btnTroLai.Size = new System.Drawing.Size(75, 44);
+            this.btnTroLai.TabIndex = 25;
+            this.btnTroLai.Text = "Trở lại";
+            this.btnTroLai.UseVisualStyleBackColor = true;
+            this.btnTroLai.Click += new System.EventHandler(this.btnTroLai_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvQuayHang);
+            this.groupBox1.Location = new System.Drawing.Point(17, 278);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(925, 195);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách Quầy hàng";
+            // 
+            // dgvQuayHang
+            // 
+            this.dgvQuayHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvQuayHang.Location = new System.Drawing.Point(6, 13);
+            this.dgvQuayHang.Name = "dgvQuayHang";
+            this.dgvQuayHang.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvQuayHang.Size = new System.Drawing.Size(913, 176);
+            this.dgvQuayHang.TabIndex = 0;
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(704, 121);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(75, 43);
+            this.btnHuy.TabIndex = 4;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // txtTenQH
+            // 
+            this.txtTenQH.Location = new System.Drawing.Point(141, 64);
+            this.txtTenQH.Name = "txtTenQH";
+            this.txtTenQH.Size = new System.Drawing.Size(202, 20);
+            this.txtTenQH.TabIndex = 7;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(556, 121);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 43);
+            this.btnLuu.TabIndex = 3;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(371, 121);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 43);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Tên Quầy Hàng";
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(215, 121);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 43);
+            this.btnSua.TabIndex = 1;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(578, 28);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(243, 20);
+            this.txtDiaChi.TabIndex = 5;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(49, 121);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 43);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(466, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Địa Chỉ";
+            // 
+            // txtMaNQL
+            // 
+            this.txtMaNQL.Location = new System.Drawing.Point(578, 64);
+            this.txtMaNQL.Name = "txtMaNQL";
+            this.txtMaNQL.Size = new System.Drawing.Size(243, 20);
+            this.txtMaNQL.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(466, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã Người Quản Lý";
+            // 
+            // txtMaQH
+            // 
+            this.txtMaQH.Location = new System.Drawing.Point(141, 25);
+            this.txtMaQH.Name = "txtMaQH";
+            this.txtMaQH.Size = new System.Drawing.Size(202, 20);
+            this.txtMaQH.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Quầy Hàng";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnHuy);
+            this.groupBox2.Controls.Add(this.txtTenQH);
+            this.groupBox2.Controls.Add(this.btnLuu);
+            this.groupBox2.Controls.Add(this.btnXoa);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btnSua);
+            this.groupBox2.Controls.Add(this.txtDiaChi);
+            this.groupBox2.Controls.Add(this.btnThem);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtMaNQL);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtMaQH);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(17, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(925, 207);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin Quầy hàng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(446, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 29);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Quầy Hàng";
+            // 
+            // frmQuayHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 557);
+            this.Controls.Add(this.btnTroLai);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label4);
+            this.Name = "frmQuayHang";
+            this.Text = "frmQuayHang";
+            this.Load += new System.EventHandler(this.frmQuayHang_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQuayHang)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTroLai;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvQuayHang;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.TextBox txtTenQH;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaNQL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMaQH;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
