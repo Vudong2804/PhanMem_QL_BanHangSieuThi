e1c15e4b0c87c4f2ecb06ca564f71aa562f8d198
VuThuy22 <vthuy2222@gmail.com>
2017-05-23 15:22:43 +0700

    khách hàng

--- QL_BanHang/QL_BanHang/Model/KhachHangMod.cs
+++ QL_BanHang/QL_BanHang/Model/KhachHangMod.cs
@@ -11,116 +11,115 @@ namespace QL_BanHang.Model
 {
     class KhachHangMod
     {
-       
-            ConnectToSql con = new ConnectToSql();
-            SqlCommand cmd = new SqlCommand();
+        ConnectToSql con = new ConnectToSql();
+        SqlCommand cmd = new SqlCommand();
 
-            public DataTable GetData()
+        public DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = "select * from KhachHang";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
             {
-                DataTable dt = new DataTable();
-                cmd.CommandText = "select * from KhachHang";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = con.strConn;
-                try
-                {
-                    con.OpenConnect();
-                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                    sda.Fill(dt);
-                    con.CloseConnection();
-                }
-                catch (Exception ex)
-                {
-                    string mes = ex.Message;
-                    cmd.Dispose();
-                    con.CloseConnection();
-                }
-                return dt;
+                con.OpenConnect();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                con.CloseConnection();
             }
-
-            public bool AddKhachHang(KhachHangObj KhObj)
+            catch (Exception ex)
             {
-                cmd.CommandText = "Insert into KhachHang values('" + KhObj.MaKH1 + "',N'" + KhObj.TenKH1 + "',CONVERT(date,'" + KhObj.NS1.ToShortDateString() + "',103)  ,N'" + KhObj.GT1 + "',N'" + KhObj.DiaChi1 + "','" + KhObj.SDT1 + "')";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = con.strConn;
-                try
-                {
-                    con.OpenConnect();
-                    cmd.ExecuteNonQuery();
-                    con.CloseConnection();
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    string mes = ex.Message;
-                    cmd.Dispose();
-                    con.CloseConnection();
-                }
-                return false;
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
             }
-
-            public bool DeleteKhachHang(string ma)
-            {
-                cmd.CommandText = "Delete KhachHang where MaKH='" + ma + "'";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = con.strConn;
-                try
-                {
-                    con.OpenConnect();
-                    cmd.ExecuteNonQuery();
-                    con.CloseConnection();
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    string mes = ex.Message;
-                    cmd.Dispose();
-                    con.CloseConnection();
-                }
-                return false;
-            }
-
-            public bool UpdateKhachHang(KhachHangObj KhObj)
-            {
-                cmd.CommandText = "Update KhachHang set TenKH=N'" + KhObj.TenKH1 + "',NS=CONVERT(date,'" + KhObj.NS1.ToShortDateString() + "',103)  ,GT=N'" + KhObj.GT1 + "',DiaChi=N'" + KhObj.DiaChi1 + "',SDT='" + KhObj.SDT1 + "' where MaKH='" + KhObj.MaKH1 + "'";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = con.strConn;
-                try
-                {
-                    con.OpenConnect();
-                    cmd.ExecuteNonQuery();
-                    con.CloseConnection();
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    string mes = ex.Message;
-                    cmd.Dispose();
-                    con.CloseConnection();
-                }
-                return false;
-            }
-            public DataTable SearchKhachHang(string MaKH)
-            {
-                DataTable dt = new DataTable();
-                cmd.CommandText = "select * from KhachHang  where MaKH like '%" + MaKH + "%'";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = con.strConn;
-                try
-                {
-                    con.OpenConnect();
-                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                    sda.Fill(dt);
-                    con.CloseConnection();
-                    return dt;
-
-                }
-                catch (Exception ex)
-                {
-                    string mes = ex.Message;
-                    cmd.Dispose();
-                    con.CloseConnection();
-                }
-                return dt;
-            }
+            return dt;
         }
+
+        public bool AddKhachHang(KhachHangObj KhObj)
+        {
+            cmd.CommandText = "Insert into KhachHang values('" + KhObj.MaKH1 + "',N'" + KhObj.TenKH1 + "',CONVERT(date,'" + KhObj.NS1.ToShortDateString() + "',103)  ,N'" + KhObj.GT1 + "',N'" + KhObj.DiaChi1 + "','" + KhObj.SDT1 + "')";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return false;
+        }
+
+        public bool DeleteKhachHang(string ma)
+        {
+            cmd.CommandText = "xoa_khachhang'" + ma + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return false;
+        }
+
+        public bool UpdateKhachHang(KhachHangObj KhObj)
+        {
+            cmd.CommandText = "Update KhachHang set TenKH=N'" + KhObj.TenKH1 + "',NS=CONVERT(date,'" + KhObj.NS1.ToShortDateString() + "',103)  ,GT=N'" + KhObj.GT1 + "',DiaChi=N'" + KhObj.DiaChi1 + "',SDT='" + KhObj.SDT1 + "' where MaKH='" + KhObj.MaKH1 + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return false;
+        }
+        public DataTable SearchKhachHang(string MaKH)
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = "select * from KhachHang  where MaKH like '%" + MaKH + "%'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                con.CloseConnection();
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return dt;
+        }
+    }
 }
